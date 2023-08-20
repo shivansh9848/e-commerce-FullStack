9b4430c236a57503f63451c8e19c296094e4ba42
Shivansh9848 <104058319+shivansh9848@users.noreply.github.com>
2023-08-20 23:59:37 +0530

    c

--- index.js
+++ index.js
@@ -188,18 +188,12 @@ passport.deserializeUser(async function (user, cb) {
 //Payments
 const stripe = stripeModule(process.env.STRIPE_SECRET_KEY);
 
-const calculateOrderAmount = (items) => {
-  // Replace this constant with a calculation of the order's amount
-  // Calculate the order total on the server to prevent
-  // people from directly manipulating the amount on the client
-  return items;
-};
-
 App.post("/create-payment-intent", async (req, res) => {
+  console.log("X",req.body)
   const {  totalAmount,orderId } = req.body;
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(totalAmount),
+    amount: totalAmount * 100,
     currency: "inr",
     // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
     automatic_payment_methods: {
